37225fc000e312f423fd1e08f97207eebbd2e9dc
Paweł Zemsta <pawel.zemsta@azimo.com>
2021-05-18 09:33:36 +0200

    Initial Commit

---
+++ TestBitriseIntegration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestBitriseIntegration
+//
+//  Created by Paweł Zemsta on 18/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestBitriseIntegration/TestBitriseIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBitriseIntegrationApp.swift
+//  TestBitriseIntegration
+//
+//  Created by Paweł Zemsta on 18/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestBitriseIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
